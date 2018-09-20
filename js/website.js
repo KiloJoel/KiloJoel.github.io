bc60413a5028c242bd67afec11aa41e949ad68fe
JOLA5 <kilojoel@gmail.com>
2018-09-20 16:08:16 +0100

    Initial commit

--- js/website.js
+++ js/website.js
@@ -1,1 +1,18 @@
- 
+console.log("test1!");
+var hidden = false;
+$("#hide-button").click(function() {
+    hidden = !hidden;
+    console.log("test2!");
+    if (hidden)
+    {
+        $("#block-of-text").show();
+    }
+    else
+    {
+        $("#block-of-text").hide();
+    }
+});
+
+function HarryFunction (){
+    console.log("We did it!");
+}
