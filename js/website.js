b5c4c13cdda6778a4dd822dac179d5239172f047
JOLA5 <kilojoel@gmail.com>
2018-09-25 16:15:22 +0100

    Made toggle buttons work

--- js/website.js
+++ js/website.js
@@ -1,3 +1,24 @@
+
+
+$("#defaultChecked2").click(function(){
+    $("#questLeftInfo").hide();
+})
+var button1 = $("input[type='checkbox'][name='check1']");
+$(button1).click(function() {
+    if ($("input[type='checkbox'][name='check1']:checked").val() == "on") {
+        console.log($("input[type='checkbox'][name='check1']:checked").val());
+        console.log("hiding");
+        $("#questLeftInfo").hide();
+    }
+    else {
+        console.log($("input[type='checkbox'][name='check1']:checked").val());
+        console.log("showing");
+        $("#questLeftInfo").show();
+    }
+})
+//$("input[type='radio'][name='rate']:checked").val();
+
+
 console.log("test1!");
 var hidden = false;
 $("#hide-button").click(function() {
@@ -19,3 +40,4 @@ $("#hide-button").click(function() {
 function HarryFunction (){
     console.log("We did it!");
 }
+
