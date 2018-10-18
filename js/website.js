01b9ced2f6734ff8fe6c77fa486f1898d8258316
JOLA5 <kilojoel@gmail.com>
2018-10-18 10:24:45 +0100

    uncommitted changes

--- js/website.js
+++ js/website.js
@@ -1,23 +1,27 @@
 
 
-$("#defaultChecked2").click(function(){
-    $("#questLeftInfo").hide();
-})
+//$("#defaultChecked2").click(function(){
+//    $("#questLeftInfo").hide();
+//})
 var button1 = $("input[type='checkbox'][name='check1']");
+var button2 = $("input[type='checkbox'][name='check2']");
 $(button1).click(function() {
     if ($("input[type='checkbox'][name='check1']:checked").val() == "on") {
-        console.log($("input[type='checkbox'][name='check1']:checked").val());
-        console.log("hiding");
         $("#questLeftInfo").hide();
     }
     else {
-        console.log($("input[type='checkbox'][name='check1']:checked").val());
-        console.log("showing");
         $("#questLeftInfo").show();
     }
 })
+$(button2).click(function() {
+    if ($("input[type='checkbox'][name='check2']:checked").val() == "on") {
+        $("#questRightInfo").hide();
+    }
+    else {
+        $("#questRightInfo").show();
+    }
+})
 //$("input[type='radio'][name='rate']:checked").val();
-
 
 console.log("test1!");
 var hidden = false;
